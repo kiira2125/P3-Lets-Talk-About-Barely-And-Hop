f4f6ef34e802c7e93c079ffbbac46d1ac9c71b6e
LK <lunakiira0911@gmail.com>
2022-08-13 11:09:59 -0700

    update loginControllers

---
+++ backend/controllers/loginController.js
@@ -0,0 +1,22 @@
+const bcrypt = require('bcrypt');
+const db = require('../backend/models');
+
+module.exports = {
+    checkUsername: (req, res) =>{
+        db.User
+            .findOne({username: req.params.username})
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(404).json(err));
+    },
+    checkLogin: (req, res) =>{
+        db.User
+        .findOne({username: req.params.username})
+        .then(users =>
+            bcrypt.compare(req.params.password, user.password).then(result =>{
+                result ? res.join({id: user._id, result: true}) 
+                : res.json({id: user._id, result: false})
+            })
+    )
+       .catch(err =>res.status(422).json(err));
+    }
+};
