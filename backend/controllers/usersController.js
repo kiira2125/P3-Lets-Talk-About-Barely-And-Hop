3a58e101cd5bc6acfe1dca615a119c24100e1301
LK <lunakiira0911@gmail.com>
2022-08-13 12:42:51 -0700

    working on my userscontrollers, on update adn remove, and install bycrypt

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -12,7 +12,42 @@ module.exports = {
             .sort({date: -1})
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
-       },
+  },
+
+    findById: (req, res) => {
+        db.User
+        .find(req.query, {password: 0})
+        .populate({path: 'recipes', populate: {path: 'brewer'}})  
+        .populate('media.followers', {password: 0})
+        .populate('media.following', {password: 0})
+        .populate({path: 'favorites', populate: {path: 'brewer'}}) 
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err));
+
+    },
+
+    create: (req, res) => {
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.has(req.body.password, salt, (err, hash) => {
+            req.body.password =hash;
+        dbUser
+          .create(req.body)
+          .then(dbModel => res.json(dbModel))
+          .catch(err => res.status(422).json(err));
+
+       });
+     });
+    },
+
+    update: (req, rew) =>{
+      dbUser
+        .update({ _id: req.params.id}, req.body, {password: 0})
+    }
 
 
+
+
+
+
+   }
         
