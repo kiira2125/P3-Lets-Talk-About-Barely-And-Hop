4574e85337d530bb49410504342a2903a55240e6
LK <lunakiira0911@gmail.com>
2022-08-16 17:31:33 -0700

    upding login.js in my route/api

--- backend/routes/api/login.js
+++ backend/routes/api/login.js
@@ -2,11 +2,13 @@ const router = require('express').Router();
 const loginController = require('../../controllers/loginController');
 
 // input the .route for username and .get check username
-router
+router.router('/:username')
+
 app.route('/check/:username')
 app.get(loginController.checkUsername);
 
 // input the .router for password and .get check login
-router
+router.router('/:password');
+
 app.route('.check/:password')
 app.get (loginController.checkLogin);
