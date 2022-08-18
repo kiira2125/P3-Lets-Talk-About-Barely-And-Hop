649bbc33ed66f08aaa2bf2cacd822732de95c04a
LK <lunakiira0911@gmail.com>
2022-08-18 19:37:39 -0400

    remove my duplicate coding for my ARB file (add recipe button .js) lol O_o

--- client/src/components/AddRecipeButton/AddRecipeButton.js
+++ client/src/components/AddRecipeButton/AddRecipeButton.js
@@ -3,9 +3,6 @@ import {Button, Grid, Col, Row} from "react-bootstrap/lib"
 import Wrapper from '../Wrapper';
 import './AddRecipeButton.css';
 
-import react from "react";
-import {Button, Grid, Col, Row} from'React-bootstrap/lib';
-import Wrapper from '../Wrapper';
 
 const AddRecipeButton = props =>{
     const LoggedIn = sessionStorage.getItem('loggedIn');
