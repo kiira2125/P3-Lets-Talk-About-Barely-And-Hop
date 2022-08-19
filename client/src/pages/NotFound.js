b00b14b1bb3e3c672ea65a1b3d1d4e62c7fab580
Robert Routhier <robrouth92@gmail.com>
2022-08-18 19:42:43 -0700

    notfound finished

---
+++ client/src/pages/NotFound.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+
+function NotFound() {
+  let location = useLocation();
+  return (
+    <div className="">
+      <div className="">
+        <h1>
+          No match for <code>{location.pathname}</code>
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default NotFound;
