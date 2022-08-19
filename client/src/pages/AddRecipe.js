b864ea946b8c9c3b8d0f5dcd7224bd0844fb8229
Robert Routhier <robrouth92@gmail.com>
2022-08-18 20:49:54 -0700

    add recipe started

---
+++ client/src/pages/AddRecipe.js
@@ -0,0 +1,14 @@
+import React, {useState} from "react";
+import AddRecipeButton from "../components/AddRecipeButton/AddRecipeButton";
+
+
+const AddRecipe = () => {
+    return (
+        <div className="">
+            <h1>Add Recipe</h1>
+            <AddRecipeButton />
+        </div>
+    );
+}
+
+export default AddRecipe;
