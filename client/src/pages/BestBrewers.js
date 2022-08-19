03f4135dbb30f4140285f9a5c44fb0f04b60be98
Robert Routhier <robrouth92@gmail.com>
2022-08-18 21:09:38 -0700

    best brewers started

---
+++ client/src/pages/BestBrewers.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import RecipeBoard from '../components/RecipeBoard/RecipeBoard';
+
+
+const BestBrewers = () => {
+    return (
+        <div>
+        <RecipeBoard />
+        </div>
+    );
+    }
+
+
+    
+export default BestBrewers;
