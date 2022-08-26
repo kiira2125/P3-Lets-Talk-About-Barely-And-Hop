47832cbbd2fc61631443c8a78d1f4f79624a0600
Robert Routhier <robrouth92@gmail.com>
2022-08-25 19:54:00 -0700

    hero carousl

---
+++ client/src/components/HeroCarousel/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+import HeroCarousel from "react-hero-carousel";
+
+export default () => (
+  <HeroCarousel>
+    <img
+      src="https://learn.kegerator.com/wp-content/uploads/2014/07/how-to-brew-beer.jpg"
+      width="100%"
+      height="100%"
+    />
+    <img
+      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6tcnmQkDKYPsHh8PIR7SL6r3O74zzQzYZtg&usqp=CAU"
+      width="100%"
+      height="100%"
+    />
+    <img
+      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwv-QSNGi2xTeb6RiLmNZyGEsGPDgyhJeN3Q&usqp=CAU"
+      width="100%"
+      height="100%"
+    />
+    <img
+      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRd281yrHWOoVUwafZrh1yr_wd_SvLxMfi0hg&usqp=CAU"
+      width="100%"
+      height="100%"
+    />
+    <img
+      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcJKPOrPiwkmGIY7WNmSWEM7NINYjANE_BwQ&usqp=CAU"
+      width="100%"
+      height="100%"
+    />
+    <img
+      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6tcnmQkDKYPsHh8PIR7SL6r3O74zzQzYZtg&usqp=CAU"
+      width="100%"
+      height="100%"
+    />
+  </HeroCarousel>
+);
+
--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -24,6 +24,7 @@ function Page({ currentPage }) {
         return <BestBrewers />;
     }
   };
+  
 
   return (
     <section>
