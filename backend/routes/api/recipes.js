7875cb12be2b497f3d8cb6d3ac32c829d92df3d0
LK <lunakiira0911@gmail.com>
2022-08-26 00:35:53 -0400

    updating again2

--- backend/routes/api/recipes.js
+++ backend/routes/api/recipes.js
@@ -2,15 +2,16 @@ const router = require('express').Router();
 const recipesController = require("../../controllers/recipesController")
 
 //getting recipe in the api using get and post
-router.route('/')
-    .get(recipesController.findAll)
-    .post(recipesController.create);
+router
+  .route('/')
+  .get(recipesController.findAll)
+  .post(recipesController.create);
 
 // getting recipe using .route (:id using), .get (findById), .put (update), and, .delete(remove)
-router.route('/:id')
-
-    .get(recipesController.findById)
-    .put(recipesController.update)
-    .delete(recipesController.remove)
+router
+  .route('/:id')
+  .get(recipesController.findById)
+  .put(recipesController.update)
+  .delete(recipesController.remove)
 
 module.exports = router;
