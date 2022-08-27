88b2bfff147bdf2a8c186df8ab073d0516e672fb
Nick Graffis <hi@nickgraffis.me>
2022-08-27 15:11:49 -0700

    new client

---
+++ client/src/App.jsx
@@ -0,0 +1,21 @@
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import Singup, { Login } from './components/Signup'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { atom } from 'jotai'
+import { AddRecipie } from './components/Blog'
+export const userAtom = atom(null)
+
+function App() {
+  return (<BrowserRouter>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/signup" element={<Singup />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/newrecipie" element={<AddRecipie />} />
+    </Routes>
+    </BrowserRouter>)
+}
+
+export default App
