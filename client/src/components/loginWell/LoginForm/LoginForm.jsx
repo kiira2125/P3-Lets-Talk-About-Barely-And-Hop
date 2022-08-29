206d6d2994f53b680b7df6ca64e30b3165c90a3b
LK <lunakiira0911@gmail.com>
2022-08-29 16:09:42 -0400

    updates on new branch

---
+++ client/src/components/loginWell/LoginForm/LoginForm.jsx
@@ -0,0 +1,6 @@
+import React, {Component} from "react";
+import {Form-Control, Row, Col, Form-Group, Button} from 'react-bootstrap';
+import API from '../../../utils/loginAPI';
+import LoginInput from './LoginInput';
+import Wrapper from '../../../components/Wrapper';
+
