23f7ed9b96734bf2671a43717261a62f1f0b47cc
LK <lunakiira0911@gmail.com>
2022-08-31 20:15:01 -0400

    RecipeCard.js updates

---
+++ client/src/components/RecipeCard/RecipeCard.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import {Col, Row, Panel, Button} from 'react-bootstrap';
+
+const RecipeCard = props =>{
+<Wrapper>
+    <Panel className="recipeCard">
+     <div className="recipeCardPanelHeading">
+     <Panel.Heading>
+      <Panel.Title toggle>
+        <h1>{props.name}</h1><h2>{props.style}</h2>
+      </Panel.Title>
+     </Panel.Heading>
+     </div>
+     <div className="panelCollapse">
+      <Panel.Collapse>
+        <Panel.Body>
+          <div className="panelBody">
+            <Row>
+             <Col xs={12} md={12}>
+              <h2>BeerBrewing: {props.Firstname} {props.LastName} - {props.brewer}</h2>
+            </Col>
+            </Row>
+            <Row>
+                <Col xs={12}>
+                    <h3> Description </h3>
+                    <p className="recipeCardScript">{props.description}</p>
+                </Col>
+            </Row>
+            <Row>
+                <Col
+            </Row>
+          </div>
+        </Panel.Body>
+      </Panel.Collapse>
+     </div>
+    </Panel>
+</Wrapper>
+}
